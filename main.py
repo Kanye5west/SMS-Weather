7b66d8c8f7e927a3b0e56d1027bbc3565f1d5f28
mr.Incomplete <153055108+Kanye5west@users.noreply.github.com>
2025-01-09 22:20:08 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+from twilio.rest import Client
+
+
+account_sid = 'acc_sid'
+auth_token = 'auth token'
+
+Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = "api_key"
+
+parameters = {
+    "lat": 14.126241,
+    "lon": 121.177266,
+    "appid": api_key,
+    "cnt": 4,
+}
+
+response = requests.get(Endpoint, params= parameters)
+weather_data = response.json()
+
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]['id']
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(body= "Bring an Umbrella!, This is from Weather SMS :)",
+                                     from_= '+15752227571',
+                                     to= '+639304170180')
+
+    print(message.sid)
